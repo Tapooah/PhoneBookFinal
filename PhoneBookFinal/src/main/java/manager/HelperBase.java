8300df63cf9d676af0cd8fa7182e129db6366e1d
Tapooah <bilyk.po@gmail.com>
2023-10-26 13:30:51 +0300

    11

--- PhoneBookFinal/src/main/java/manager/HelperBase.java
+++ PhoneBookFinal/src/main/java/manager/HelperBase.java
@@ -1,6 +1,8 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HelperBase {
     WebDriver wd;
@@ -10,4 +12,15 @@ public class HelperBase {
     }
 
 
+    ////////////////////////////////////// ˅ service methods ˅ //////////////////////////////////////
+    public void type(By locator, String text) {
+        //find + click + clear + sendKey
+        if (text != null) {
+            WebElement elem = wd.findElement(locator);
+            elem.click();
+            elem.clear();
+            elem.sendKeys(text);
+        }
+    }
+    ////////////////////////////////////// ˄ service methods ˄ //////////////////////////////////////
 }
--- PhoneBookFinal/src/test/java/tests/TestBase.java
+++ PhoneBookFinal/src/test/java/tests/TestBase.java
@@ -27,37 +27,4 @@ public class TestBase {
     }
 
 
-    ////////////////////////////////////// ˅ service methods ˅ //////////////////////////////////////
-
-    public void openLoginRegistrationForm() {
-        WebElement loginTab = wd.findElement(By.cssSelector("a[href='/login']"));
-        loginTab.click();
-    }
-
-    public void submitLogin() {
-        wd.findElement(By.xpath("//button[1]")).click();
-    }
-
-    public void submitRegistration() {
-        wd.findElement(By.xpath("//button[2]")).click();
-    }
-
-    public void fillLoginRegistrationForm(String email, String password) {
-        //find + click + clear + sendKey
-        type(By.xpath("//input[1]"), email);
-        type(By.xpath("//input[2]"), password);
-    }
-
-    public void type(By locator, String text) {
-        //find + click + clear + sendKey
-        if (text != null) {
-            WebElement elem = wd.findElement(locator);
-            elem.click();
-            elem.clear();
-            elem.sendKeys(text);
-        }
-    }
-
-
-    ////////////////////////////////////// ˄ service methods ˄ //////////////////////////////////////
 }
